b9e186542bb98a44f7f3f7c138fe4b484a0d5c65
mmarcosa <marcos.barbosa8@fatec.sp.gov.br>
2020-05-15 03:27:16 -0300

    refatoracoes

--- src/main/java/com/producer/demo/jms/Producer.java
+++ src/main/java/com/producer/demo/jms/Producer.java
@@ -1,48 +1,26 @@
 package com.producer.demo.jms;
 
 import com.producer.demo.model.Person;
-import org.springframework.scheduling.annotation.Scheduled;
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.jms.core.JmsTemplate;
 import org.springframework.stereotype.Component;
-
-import javax.jms.*;
-import javax.naming.InitialContext;
-import javax.naming.NamingException;
-
 import com.google.gson.Gson;
 
+@Component
+@RequiredArgsConstructor
 public class Producer {
 
-    private Session session;
-    private Connection connection;
-    private InitialContext context;
-    private ConnectionFactory factory;
+    private final JmsTemplate jmsTemplate;
 
-    public void connectToRow(Person person) throws NamingException, JMSException {
-        openConnection();
-        Destination fila = (Destination) context.lookup("pessoa");
-        MessageProducer producer = session.createProducer(fila);
+    @Value("${activemq.name}")
+    private String destinationQueue;
+
+    public void send(Person person){
         Gson gson = new Gson();
         String jsonPerson = gson.toJson(person);
-        Message mensagem = session.createTextMessage(jsonPerson);
-        producer.send(mensagem);
-        closeConnection();
+        jmsTemplate.convertAndSend(destinationQueue, jsonPerson);
     }
-
-    private void openConnection() throws JMSException, NamingException{
-        context = new InitialContext();
-        factory = (ConnectionFactory) context.lookup("ConnectionFactory");
-        connection = factory.createConnection();
-        connection.start();
-        session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
-    }
-
-
-    private void closeConnection() throws JMSException, NamingException {
-        session.close();
-        connection.close();
-        context.close();
-    }
-
-
 
 }
